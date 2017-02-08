5b23506c414606036e51323a421684fa9c75cefa
Zach Zador <zazador@gmail.com>
2017-02-08 17:40:20 -0600

    external js linked

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	var pokemon = ["bulbasaur","ivysaur","venusaur","charmander","charmeleon","charizard",
+	"squirtle","wartortle","blastoise"]
+
+	$(".number").on("click", function() {
+		alert("hi");
+
+	});
+
+});
